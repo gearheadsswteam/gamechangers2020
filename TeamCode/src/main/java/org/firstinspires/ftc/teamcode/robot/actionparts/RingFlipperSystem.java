a5bb4167b012c206c1372ebe698d5bd6032b0bc1
Sidharth Rane <hrishi.rane@gmail.com>
2020-10-17 09:47:48 -0500

    Intake and shooting system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/actionparts/RingFlipperSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/actionparts/RingFlipperSystem.java
@@ -1,31 +1,34 @@
 package org.firstinspires.ftc.teamcode.robot.actionparts;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.robot.GearheadsMecanumRobot;
 
-public class FoundationGrabber {
+public class RingFlipperSystem {
 
+    public Servo leftServo;
+    public Servo rightServo;
     public static double  MIN_POSITION = 0.0, MAX_POSITION = 1, MIDDLE_POSITION = 0.5;
     public static double  INIT_POSITION = 0.0, GRAB_POSITION = 0.18, UNGRAB_POSITION = 0.0;
 
     GearheadsMecanumRobot myRobot;
     LinearOpMode curOpMode;
 
-    public FoundationGrabber(LinearOpMode opMode, GearheadsMecanumRobot robot){
+    public RingFlipperSystem(LinearOpMode opMode, Servo leftRobotServo,Servo rightRobotServo){
         curOpMode = opMode;
-        myRobot = robot;
-
+        leftServo = leftRobotServo;
+        rightServo = rightRobotServo;
     }
 
-    public void grabFoundation(){
+    public void flipRings(){
         curOpMode.telemetry.addData("Grab = ", "");
         curOpMode.telemetry.update();
         operateServo(GRAB_POSITION);
 
     }
-    public void ungrabFoundation(){;
+    public void unflipRings(){;
         curOpMode.telemetry.addData("unGrab = ", "");
         curOpMode.telemetry.update();
         operateServo(UNGRAB_POSITION);
@@ -33,15 +36,15 @@ public class FoundationGrabber {
 
     public double operateServo(double gripPosition) {
         // open the gripper on X button if not already at most open position.
-        myRobot.leftFoundationServo.setPosition(Range.clip(gripPosition, FoundationGrabber.MIN_POSITION, FoundationGrabber.MAX_POSITION));
-        myRobot.rightFoundationServo.setPosition(Range.clip(gripPosition, FoundationGrabber.MIN_POSITION, FoundationGrabber.MAX_POSITION));
+        leftServo.setPosition(Range.clip(gripPosition, RingFlipperSystem.MIN_POSITION, RingFlipperSystem.MAX_POSITION));
+        rightServo.setPosition(Range.clip(gripPosition, RingFlipperSystem.MIN_POSITION, RingFlipperSystem.MAX_POSITION));
         printSkystoneServoState();
         return gripPosition;
     }
 
     public void printSkystoneServoState() {
-        curOpMode.telemetry.addData("leftFoundationServo val = ", myRobot.leftFoundationServo.getPosition());
-        curOpMode.telemetry.addData("rightFoundationServo val = ", myRobot.rightFoundationServo.getPosition());
+        curOpMode.telemetry.addData("leftFoundationServo val = ", leftServo.getPosition());
+        curOpMode.telemetry.addData("rightFoundationServo val = ", rightServo.getPosition());
         curOpMode.telemetry.update();
     }
 }
