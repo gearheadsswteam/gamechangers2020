dffb78c1e88563a1127c9bbe5f18362a442a97f0
Rishi Rane <hrishi.rane@gmail.com>
2020-10-25 15:42:50 -0500

    Some experiments with Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ConceptScanServo.java
@@ -38,12 +38,12 @@ import com.qualcomm.robotcore.hardware.Servo;
  * The code is structured as a LinearOpMode
  * INCREMENT sets how much to increase/decrease the servo position each cycle
  * CYCLE_MS sets the update period.
- *
+ * <p>
  * This code assumes a Servo configured with the name "left_hand" as is found on a pushbot.
- *
+ * <p>
  * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
  * connected servos are able to move freely before running this test.
- *
+ * <p>
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
@@ -51,14 +51,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 //@Disabled
 public class ConceptScanServo extends LinearOpMode {
 
-    static final double INCREMENT   = 0.05;     // amount to slew servo each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   500;     // period of each cycle
-    static final double MAX_POS     =  1.0;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;     // Minimum rotational position
+    static final double INCREMENT = 0.05;     // amount to slew servo each CYCLE_MS cycle
+    static final int CYCLE_MS = 500;     // period of each cycle
+    static final double MAX_POS = 0.8;     // Maximum rotational position
+    static final double MIN_POS = 0.4;     // Minimum rotational position
 
     // Define class members
     Servo servo;
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    //double position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double position;
     boolean rampUp = true;
 
 
@@ -69,43 +70,50 @@ public class ConceptScanServo extends LinearOpMode {
         // Change the text in quotes to match any servo name on your robot.
         servo = hardwareMap.get(Servo.class, "rightFlipper");
         servo.setDirection(Servo.Direction.REVERSE);//For the "rightFlipper"
+        position = 0.5;
+        servo.setPosition(position);
+        sleep(300);
 
         // Wait for the start button
-        telemetry.addData(">", "Press Gamepad 1 X to scan Servo" );
+        telemetry.addData(">", "Press Gamepad 1 X to scan Servo");
         telemetry.update();
         waitForStart();
 
 
         // Scan servo till stop pressed.
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
             position = servo.getPosition();
             // Display the current value
             telemetry.addData("Servo Initial Position", "%5.2f", position);
             telemetry.update();
             sleep(500);
 
-            if(gamepad1.x) {
-                // slew the servo, according to the rampUp (direction) variable.
-                if (rampUp) {
-                    // Keep stepping up until we hit the max value.
-                    position += INCREMENT;
-                    if (position >= MAX_POS) {
-                        position = MAX_POS;
-                        rampUp = !rampUp;   // Switch ramp direction
-                    }
-                } else {
-                    // Keep stepping down until we hit the min value.
-                    position -= INCREMENT;
-                    if (position <= MIN_POS) {
-                        position = MIN_POS;
-                        rampUp = !rampUp;  // Switch ramp direction
-                    }
+            if (gamepad1.right_bumper) {
+                // Keep stepping up until we hit the max value.
+                position += INCREMENT;
+                if (position >= MAX_POS) {
+                    position = MAX_POS;
+                    telemetry.addData("Reached MAX pos", "%5.2f", position);
+                    telemetry.update();
                 }
+                // Set the servo to the new position and pause;
+                servo.setPosition(position);
+                sleep(50);
+            }
+            if (gamepad1.left_bumper) {
+                // Keep stepping down until we hit the min value.
+                position -= INCREMENT;
+                if (position <= MIN_POS) {
+                    position = MIN_POS;
+                    telemetry.addData("Reached Min pos", "%5.2f", position);
+                    telemetry.update();
+                }
+                // Set the servo to the new position and pause;
+                servo.setPosition(position);
+                sleep(50);
             }
 
-            // Set the servo to the new position and pause;
-            servo.setPosition(position);
-            sleep(50);
+
             // Display the current value
             telemetry.addData("Servo Position", "%5.2f", servo.getPosition());
             telemetry.update();
@@ -113,9 +121,5 @@ public class ConceptScanServo extends LinearOpMode {
             sleep(CYCLE_MS);
             idle();
         }
-
-        // Signal done;
-        telemetry.addData(">", "Done");
-        telemetry.update();
     }
 }
