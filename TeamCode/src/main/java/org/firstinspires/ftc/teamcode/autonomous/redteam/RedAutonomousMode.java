dc05680fa51002af0c92b917a77e776669e07b86
Rishi Rane <hrishi.rane@gmail.com>
2020-11-28 14:01:06 -0600

    Basic Autonomous code added.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/redteam/RedAutonomousMode.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.autonomous.redteam;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.autonomous.AbstractAutonomousOpMode;
+import org.firstinspires.ftc.teamcode.autonomous.blueteam.BlueRingCase0AutonomousOpMode;
+import org.firstinspires.ftc.teamcode.autonomous.blueteam.BlueRingCase1AutonomousOpMode;
+import org.firstinspires.ftc.teamcode.autonomous.blueteam.BlueRingCase4AutonomousOpMode;
+
+@Autonomous(name = "RedAutonomousMode", group = "Mecannum")
+
+public class RedAutonomousMode extends AbstractAutonomousOpMode {
+    @Override
+    protected void initOpModeAfterStart() {
+
+    }
+
+    @Override
+    protected void executeOpMode() {
+        int rings = robot.ringDetector.detectRings();
+        telemetry.addData("Rings ", rings);
+        telemetry.update();
+
+        if(rings == 0){
+            ///execute Rings = 0 case
+            RedRingCase0AutonomousOpMode ringCase0AutonomousOpMode = new RedRingCase0AutonomousOpMode();
+            ringCase0AutonomousOpMode.executeOpMode();
+        }else if(rings == 1){
+            ///execute Rings = 1 case
+            RedRingCase1AutonomousOpMode ringCase0AutonomousOpMode = new RedRingCase1AutonomousOpMode();
+            ringCase0AutonomousOpMode.executeOpMode();
+        }else if(rings == 4){
+            ///execute Rings = 4 case
+            RedRingCase4AutonomousOpMode ringCase0AutonomousOpMode = new RedRingCase4AutonomousOpMode();
+            ringCase0AutonomousOpMode.executeOpMode();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/redteam/RedRingCase1AutonomousOpMode.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.autonomous.redteam;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.autonomous.AbstractAutonomousOpMode;
+
+public class RedRingCase1AutonomousOpMode extends AbstractAutonomousOpMode {
+
+    @Override
+    protected void initOpModeAfterStart() {
+
+    }
+
+    @Override
+    public void executeOpMode() {
+        moveFirstWobbleGoal();
+        gotoShootingPosition();
+        shootPreloadedRings();
+        grabRings();
+        shootPreloadedRings();
+        //gotoToSecondWobbleGoal();
+        moveSecondWobbleGoal();
+        park();
+    }
+
+    private void moveFirstWobbleGoal(){
+        autonomousRobotMover.rotateLeft(15, 0.1);
+        autonomousRobotMover.moveRobotBackwardDistance(0.3,40);
+    }
+
+    private void gotoShootingPosition(){
+        autonomousRobotMover.moveRobotForwardDistance(0.3,6);
+        autonomousRobotMover.rotateRight(15, 0.1);
+        autonomousRobotMover.moveRobotLeftDistance(0.2,9);
+        autonomousRobotMover.moveRobotLeftDistance(0.2,9);
+    }
+
+    private void gotoToSecondWobbleGoal(){
+
+    }
+
+    private void shootPreloadedRings(){
+        autonomousRobotMover.robot.shootingSystem.operateShooterMotor(1);
+        sleep(500);
+        autonomousRobotMover.robot.ringFlipperSystem.pushRing();
+        sleep(500);
+        autonomousRobotMover.robot.ringFlipperSystem.pushRing();
+        sleep(500);
+        autonomousRobotMover.robot.ringFlipperSystem.pushRing();
+        sleep(500);
+        autonomousRobotMover.robot.shootingSystem.stopShooterMotor();
+    }
+
+    private void grabRings(){
+        autonomousRobotMover.moveRobotForwardDistance(0.3,10);
+        autonomousRobotMover.robot.intakesystem.startInTake();
+        sleep(5000);
+        autonomousRobotMover.robot.intakesystem.stopInTake();
+        autonomousRobotMover.moveRobotBackwardDistance(0.3,10);
+    }
+
+
+
+    private void moveSecondWobbleGoal(){
+
+    }
+
+    private void park(){
+        autonomousRobotMover.moveRobotBackwardDistance(0.5,6);
+    }
+}
