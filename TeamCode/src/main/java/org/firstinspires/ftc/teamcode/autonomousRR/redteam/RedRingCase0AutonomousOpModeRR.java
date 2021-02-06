7f11d4adff37a3b2eda52a8d72ba07eec3437115
Rishi Rane <hrishi.rane@gmail.com>
2021-02-06 13:51:47 -0600

    Case 0 working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/redteam/RedRingCase0AutonomousOpModeRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/redteam/RedRingCase0AutonomousOpModeRR.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.autonomousRR.redteam;
 
+import android.provider.MediaStore;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -45,14 +47,14 @@ public class RedRingCase0AutonomousOpModeRR {
 
     public void executeOpMode() {
 
-        Trajectory traj1 = mecanumDriveRR.trajectoryBuilder(initPos, 0).splineTo(new Vector2d(2, -56), -0.4)
+        Trajectory traj1 = mecanumDriveRR.trajectoryBuilder(initPos, 0).splineTo(new Vector2d(2, -50), -0.4)
                 .build();
 
-        Trajectory traj2 = mecanumDriveRR.trajectoryBuilder(new Pose2d(2, -56, -0.4), Math.PI - 0.4)
-                .splineToLinearHeading(new Pose2d(-2, -36, 0.35), 0.55)//Shooting angle
+        Trajectory traj2 = mecanumDriveRR.trajectoryBuilder(traj1.end(), Math.PI - 0.4)
+                .splineToLinearHeading(new Pose2d(-2, -36, 0.35), 0.55)//Shooting angle was 0.65
                 .build();
 
-        Trajectory traj3 = mecanumDriveRR.trajectoryBuilder(new Pose2d(-2, -36, 0.55), 0.55)
+        Trajectory traj3 = mecanumDriveRR.trajectoryBuilder(traj2.end(), 0.75)
                 .splineTo(new Vector2d(-56, -11), -Math.PI / 3).build();
 
         currOpMode.sleep(500);
@@ -61,7 +63,7 @@ public class RedRingCase0AutonomousOpModeRR {
         TrajectoryConstraints slowConstraints = new MecanumConstraints(DriveConstants.SLOW_ROBOT_CONSTRAINTS, DriveConstants.TRACK_WIDTH);
         Trajectory traj4 = mecanumDriveRR.trajectoryBuilder(traj3.end())
                 .splineTo(new Vector2d(-47.5, -21.5), -Math.PI / 3, slowConstraints)
-                .splineTo(new Vector2d(7, -50), 0, slowConstraints).build();
+                .splineTo(new Vector2d(7, -44), 0, slowConstraints).build();
 
 
         shootingSystem.operateShooterMotors(0.15, 0.075);
@@ -84,7 +86,18 @@ public class RedRingCase0AutonomousOpModeRR {
         shootingSystem.stopShooterMotor();
         mecanumDriveRR.followTrajectory(traj3);
         mecanumDriveRR.followTrajectory(traj4);
+
+        //Park and set for TeleOps
+//        Trajectory traj5 = mecanumDriveRR.trajectoryBuilder(traj4.end())
+//                .back(6).build();
+//        mecanumDriveRR.followTrajectory(traj5);
+//        mecanumDriveRR.turn(Math.toRadians(90));
+//
+//        Trajectory traj6 = mecanumDriveRR.trajectoryBuilder(traj5.end())
+//                .strafeRight(6).build();
+//        mecanumDriveRR.followTrajectory(traj6);
     }
+
 
 
 }
