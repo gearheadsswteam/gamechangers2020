1867fd0caacf48b89eb609e9d3bedd142beeab18
Rishi Rane <hrishi.rane@gmail.com>
2021-02-20 15:01:38 -0600

    cASE 4 WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/redteam/RedRingCase4AutonomousOpModeRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/redteam/RedRingCase4AutonomousOpModeRR.java
@@ -47,40 +47,50 @@ public class RedRingCase4AutonomousOpModeRR {
     }
 
     public void executeOpMode() {
-        mecanumDriveRR.setPoseEstimate(initPos);
-
-        //From Starting position to Case 0 drop zone
+        //Clear the ring set up position
         Trajectory traj1 = mecanumDriveRR.trajectoryBuilder(initPos, 0)
-                .splineTo(new Vector2d(36, -48), 0)
-                .splineToSplineHeading(RedTeamPositions.SHOOTING_POS, 0)//Shooting angle was 0.65
+                .splineTo(new Vector2d(-24, -56), 0)
+                .splineTo(new Vector2d(4.97-3+ 48, -59.69+ 2), 5.6)
                 .build();
+
+
+
+        //From Case 1 drop zone to shooting position
+        Trajectory traj2 = mecanumDriveRR.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(RedTeamPositions.SHOOTING_POS_CASE_1)
+                .build();
+
 
         //Shooting position to wobble goal 2 grab position
-        Trajectory traj3 = mecanumDriveRR.trajectoryBuilder(RedTeamPositions.SHOOTING_POS, 0)
+        Trajectory traj3 = mecanumDriveRR.trajectoryBuilder(traj2.end(), 0)
                 .splineTo(new Vector2d(-39.6, 9.1), 3.3)
                 //.splineTo(new Vector2d(-56.37, -5.82), 4.75)  //Newly added point
-                .splineTo(new Vector2d(-57.52+6, -7.70), 5.0)
+                .splineTo(new Vector2d(-57.52+6, -7.70+3+2), 5.0)
                 .build();
 
-//
-//        //To slow down robot, from Wobble goal 2 catch position to Case 0 drop position
+
+        //To slow down robot, from Wobble goal 2 catch position to Case 0 drop position
         TrajectoryConstraints slowConstraints = new MecanumConstraints(DriveConstants.SLOW_ROBOT_CONSTRAINTS, DriveConstants.TRACK_WIDTH);
         Trajectory traj4 = mecanumDriveRR.trajectoryBuilder(traj3.end())
-                .splineTo(new Vector2d(-51.3+2, -17.56), 5.0, slowConstraints)
-                .splineTo(new Vector2d(3.76+ 48, -62.76), 0, slowConstraints).build();//
+                .lineTo(new Vector2d(-51.3, -17.56), slowConstraints)
+                .splineTo(new Vector2d(3.76-3+48, -62.76), 0, slowConstraints).build();
+
 
         shootingSystem.operateShooterMotors(0.15, 0.075);
         ringFlipperSystem.resetPosition();
 
 
         mecanumDriveRR.followTrajectory(traj1);
-        //mecanumDriveRR.followTrajectory(traj2);
+        mecanumDriveRR.followTrajectory(traj2);
+
 
         ringFlipperSystem.pushRing();
         currOpMode.sleep(500);
         ringFlipperSystem.pushRing();
         currOpMode.sleep(500);
         ringFlipperSystem.pushRing();
+
+        //grabAndShootRings(traj2);
 
         shootingSystem.stopShooterMotor();
         mecanumDriveRR.followTrajectory(traj3);
@@ -88,7 +98,7 @@ public class RedRingCase4AutonomousOpModeRR {
         mecanumDriveRR.followTrajectory(traj4);
 
         Trajectory traj5 = mecanumDriveRR.trajectoryBuilder(traj4.end())
-                .back(48).build();
+                .back(24).build();
         mecanumDriveRR.followTrajectory(traj5);
     }
 }
