dfbf44c9662eba4f221723d47c9811c9b52da9dc
Rishi Rane <hrishi.rane@gmail.com>
2021-02-21 14:34:27 -0600

    Blue positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamPositions.java
@@ -6,11 +6,12 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import org.firstinspires.ftc.teamcode.drive.PoseStorage;
 
 public class BlueTeamPositions {
-    public static final Pose2d INIT_POS = PoseStorage.DEFAULT_AUTONOMOUS_INIT_POSITION;
-    public static final Pose2d SHOOTING_POS = new Pose2d(4.21, -36.82+72, 0);
-    public static final Pose2d SHOOTING_POS_CASE_1 = new Pose2d(2.21, -36.82, 0);
-    public static final Pose2d PARK_POS_CASE_4 = new Pose2d(4.21, -36.82, 0);
-    public static final Vector2d RING_GRAB_POS = new Vector2d(-33.52,-19.7);
-    public static final Vector2d WOBBLE_GOAL_2_PICKUP_XY= new Vector2d(-57.52, -7.70);
-    public static final double WOBBLE_GOAL_2_PICKUP_HEADING= 5.121;
+    public static final Pose2d INIT_POS = new Pose2d(-60, 48, 0);
+    public static final Pose2d SHOOTING_POS = new Pose2d(4.21, 36.82, 0);
+    public static final Pose2d SHOOTING_POS_CASE_1 = new Pose2d(2.21, 36.82, 0);
+    public static final Pose2d SHOOTING_POS_CASE_4 = new Pose2d(2.21, 38.82, 0);
+    public static final Pose2d PARK_POS_CASE_4 = new Pose2d(6.21, 36.82, 0);
+    public static final Vector2d RING_GRAB_POS = new Vector2d(-33.52,19.7);
+    public static final Vector2d WOBBLE_GOAL_2_PICKUP_XY= new Vector2d(-57.52, 7.70);
+    public static final double WOBBLE_GOAL_2_PICKUP_HEADING= Math.PI/2-5.121;
 }
