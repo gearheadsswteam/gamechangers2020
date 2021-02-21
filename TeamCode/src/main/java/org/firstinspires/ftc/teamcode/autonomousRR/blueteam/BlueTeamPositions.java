e45affb2fa454203396d354dbc18eef6ac0c6961
Rishi Rane <hrishi.rane@gmail.com>
2021-02-21 11:16:15 -0600

    BlueTeam Positions class moved.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamPositions.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autonomousRR;
+package org.firstinspires.ftc.teamcode.autonomousRR.blueteam;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
