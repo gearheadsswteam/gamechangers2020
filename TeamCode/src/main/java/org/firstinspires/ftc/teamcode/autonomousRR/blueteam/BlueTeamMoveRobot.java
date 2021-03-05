8fd08624e7b2902378f660763bf6de6749083fb6
Rishi Rane <hrishi.rane@gmail.com>
2021-03-04 21:00:59 -0600

    Position fixed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamMoveRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRR/blueteam/BlueTeamMoveRobot.java
@@ -9,11 +9,11 @@ import org.firstinspires.ftc.teamcode.autonomousRR.AbstractAutonomousOpModeRR;
 @Autonomous
 public class BlueTeamMoveRobot extends AbstractAutonomousOpModeRR {
     private Pose2d initPos = BlueTeamPositions.WG2_START_POS;
-    private Pose2d destPos = new Pose2d(12, 24, 0);
+    private Pose2d destPos = BlueTeamPositions.SHOOTING_POS;
     private long delayMs = 5000;
 
     public BlueTeamMoveRobot() {
-        super.TEAM_TYPE = AbstractAutonomousOpModeRR.RED_TEAM;
+        super.TEAM_TYPE = AbstractAutonomousOpModeRR.BLUE_TEAM;
     }
     @Override
     protected void initOpModeBeforeStart() {
@@ -35,8 +35,22 @@ public class BlueTeamMoveRobot extends AbstractAutonomousOpModeRR {
         Trajectory traj1 = mecanumDriveRR.trajectoryBuilder(initPos, 0)
                 .splineToSplineHeading(destPos, 0)
                 .build();
+
+        Trajectory traj2 = mecanumDriveRR.trajectoryBuilder(traj1.end(), 0)
+                .forward(6).build();
+
+        robot.shootingSystem.startShooterMotor();
         this.sleep(delayMs);
         mecanumDriveRR.followTrajectory(traj1);
 
+        robot.ringFlipperSystem.pushRing();
+        sleep(500);
+        robot.ringFlipperSystem.pushRing();
+        sleep(500);
+        robot.ringFlipperSystem.pushRing();
+
+        mecanumDriveRR.followTrajectory(traj2);
+
+        robot.shootingSystem.stopShooterMotor();
     }
 }
